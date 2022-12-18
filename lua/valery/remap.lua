f63bac255ce0527d39ee0f3ba9e5d9569e24a473
Bergman Valery <bergman.vd@yandex.ru>
2022-12-18 18:59:56 +0300

    Move from Explore to Texplore because of convenience

--- lua/valery/remap.lua
+++ lua/valery/remap.lua
@@ -3,7 +3,7 @@ vim.keymap.set("i", "jk", "<Esc>")
 -- greatest remap ever
 vim.keymap.set("x", "<leader>p", "\"_dP")
 -- go to explorer
-vim.keymap.set("n", "<leader>e", "<cmd>Ex<CR>")
+vim.keymap.set("n", "<leader>e", "<cmd>Texplore<CR>")
 -- write changes
 vim.keymap.set("n", "<leader>w", "<cmd>w<CR>")
 -- toggle highlighting
