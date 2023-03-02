5e4c9fd40270fc1926bd414ba0dd197d25e5c946
Bergman Valery <bergman.vd@yandex.ru>
2023-03-02 23:36:00 +0300

    Add j-k associations in other variants of registers

--- lua/valery/remap.lua
+++ lua/valery/remap.lua
@@ -1,5 +1,8 @@
 -- jk for <Esc>
 vim.keymap.set("i", "jk", "<Esc>")
+vim.keymap.set("i", "Jk", "<Esc>")
+vim.keymap.set("i", "jK", "<Esc>")
+vim.keymap.set("i", "JK", "<Esc>")
 -- greatest remap ever
 vim.keymap.set("x", "<leader>p", "\"_dP")
 -- go to explorer
